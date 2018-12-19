b9c17b50bb6d1067e86b1f93695f46ee5e6ee478
Radu Mazilu <maziluradugabriel@gmail.com>
2018-12-19 17:13:08 +0000

    added constants file

--- src/components/navigation-components/SectionPills.js
+++ src/components/navigation-components/SectionPills.js
@@ -3,14 +3,11 @@ import React, { Component } from "react";
 // @material-ui/core components
 import withStyles from "@material-ui/core/styles/withStyles";
 
-// @material-ui/icons
-import Dashboard from "@material-ui/icons/Dashboard";
-import Schedule from "@material-ui/icons/Schedule";
-import List from "@material-ui/icons/List";
-
 // core components
 import NavPills from "./NavPills";
 import $ from 'jquery';
+
+import { guvernantaContent } from '../../config/constants';
 
 class SectionPills extends Component {
 
@@ -26,91 +23,7 @@ class SectionPills extends Component {
       <div id="navigation-pills">
         <NavPills
           color="primary"
-          tabs={[
-            {
-              tabButton: "Acte Necesare",
-              tabIcon: Dashboard,
-              tabContent: (
-                <span>
-                  <p>
-                    Collaboratively administrate empowered markets via
-                    plug-and-play networks. Dynamically procrastinate
-                    B2C users after installed base benefits.
-                  </p>
-                </span>
-              )
-            },
-            {
-              tabButton: "Proiecte",
-              tabIcon: Schedule,
-              tabContent: (
-                <span>
-                  <li>
-                    Efficiently unleash cross-media information without
-                    cross-media value. Quickly maximize timely
-                    deliverables for real-time schemas.
-                  </li>
-                  <li>
-                    Efficiently unleash cross-media information without
-                    cross-media value. Quickly maximize timely
-                    deliverables for real-time schemas.
-                  </li>
-                </span>
-              )
-            },
-            {
-              tabButton: "Informatii publice",
-              tabIcon: List,
-              tabContent: (
-                <span>
-                  <p>
-                    Collaboratively administrate empowered markets via
-                    plug-and-play networks. Dynamically procrastinate
-                    B2C users after installed base benefits.
-                  </p>
-                </span>
-              )
-            },
-            {
-              tabButton: "Buget",
-              tabIcon: List,
-              tabContent: (
-                <span>
-                  <p>
-                    Collaboratively administrate empowered markets via
-                    plug-and-play networks. Dynamically procrastinate
-                    B2C users after installed base benefits.
-                  </p>
-                </span>
-              )
-            },
-            {
-              tabButton: "Program cu publicul",
-              tabIcon: List,
-              tabContent: (
-                <span>
-                  <p>
-                    Collaboratively administrate empowered markets via
-                    plug-and-play networks. Dynamically procrastinate
-                    B2C users after installed base benefits.
-                  </p>
-                </span>
-              )
-            },
-            {
-              tabButton: "Anunturi",
-              tabIcon: List,
-              tabContent: (
-                <span>
-                  <p>
-                    Collaboratively administrate empowered markets via
-                    plug-and-play networks. Dynamically procrastinate
-                    B2C users after installed base benefits.
-                  </p>
-                </span>
-              )
-            }
-          ]}
+          tabs={guvernantaContent}
         />
       </div>
     );
---
+++ src/config/constants.js
@@ -0,0 +1,90 @@
+import React from 'react';
+
+// @material-ui/icons
+import Dashboard from "@material-ui/icons/Dashboard";
+import Schedule from "@material-ui/icons/Schedule";
+import List from "@material-ui/icons/List";
+
+export const guvernantaContent = [{
+  tabButton: "Acte Necesare",
+  tabIcon: Dashboard,
+  tabContent: (
+    <span>
+      <p>
+        Collaboratively administrate empowered markets via
+        plug-and-play networks. Dynamically procrastinate
+        B2C users after installed base benefits.
+      </p>
+    </span>
+  )
+},
+{
+  tabButton: "Proiecte",
+  tabIcon: Schedule,
+  tabContent: (
+    <span>
+      <li>
+        Efficiently unleash cross-media information without
+        cross-media value. Quickly maximize timely
+        deliverables for real-time schemas.
+      </li>
+      <li>
+        Efficiently unleash cross-media information without
+        cross-media value. Quickly maximize timely
+        deliverables for real-time schemas.
+      </li>
+    </span>
+  )
+},
+{
+  tabButton: "Informatii publice",
+  tabIcon: List,
+  tabContent: (
+    <span>
+      <p>
+        Collaboratively administrate empowered markets via
+        plug-and-play networks. Dynamically procrastinate
+        B2C users after installed base benefits.
+      </p>
+    </span>
+  )
+},
+{
+  tabButton: "Buget",
+  tabIcon: List,
+  tabContent: (
+    <span>
+      <p>
+        Collaboratively administrate empowered markets via
+        plug-and-play networks. Dynamically procrastinate
+        B2C users after installed base benefits.
+      </p>
+    </span>
+  )
+},
+{
+  tabButton: "Program cu publicul",
+  tabIcon: List,
+  tabContent: (
+    <span>
+      <p>
+        Collaboratively administrate empowered markets via
+        plug-and-play networks. Dynamically procrastinate
+        B2C users after installed base benefits.
+      </p>
+    </span>
+  )
+},
+{
+  tabButton: "Anunturi",
+  tabIcon: List,
+  tabContent: (
+    <span>
+      <p>
+        Collaboratively administrate empowered markets via
+        plug-and-play networks. Dynamically procrastinate
+        B2C users after installed base benefits.
+      </p>
+    </span>
+  )
+}]
